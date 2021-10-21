3523e8c93c24f290e5be35954e25f051519fe8c7
Ruta R <rutareiso@protonmail.com>
2021-10-21 12:26:22 -0400

    Add solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
-#write your code here
+def translate(str)
+  words = str.split(' ')
+  new_words = words.map { |word| pig_latinize(word) }
+  new_words.join(' ')
+end
+
+def pig_latinize(word)
+  word = word.downcase
+  vowels = %w[a e i o u]
+  return "#{word}ay" if vowels.include?(word[0])
+
+  consonants = word.match(/[^aeiou]+/)[0]
+  consonants += 'u' if consonants[-1] == 'q' && word[consonants.length] == 'u'
+  word[0...consonants.length] = ''
+  "#{word}#{consonants}ay"
+end
+
